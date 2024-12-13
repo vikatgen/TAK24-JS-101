4a7238534842defff9f00e5c5739915eb47fa119
Gen Vikat <vikat.gen@gmail.com>
2024-12-13 13:17:27 +0200

    adds feature to push new items to array trough form submit event

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const welcomeButton = document.querySelector('#welcome-button');
 const fruitListElement = document.querySelector('.fruit-list');
+const fruitForm = document.querySelector("#add-new-fruit-form");
+const errorMessageSpan = document.querySelector("#error-message");
 
-const fruits = ["Apple", "Banana", "Mango", "Kiwi"];
+let fruits = ["Apple", "Banana", "Mango", "Kiwi"];
+
 const olElement = document.createElement('ol');
-for(let i = 0; i < fruits.length; i++) {
-    const listElement = document.createElement('li');
-    listElement.innerText = fruits[i];
-    olElement.appendChild(listElement);
-}
+
+function renderFruitList() {
+    olElement.innerHTML = "";
+    for(let i = 0; i < fruits.length; i++) {
+        const listElement = document.createElement('li');
+        listElement.innerText = fruits[i];
+        olElement.appendChild(listElement);
+    }
+};
 fruitListElement.appendChild(olElement);
+
+renderFruitList();
 
 
 
@@ -16,4 +25,23 @@ welcomeButton.classList.add('primary-button');
 welcomeButton.addEventListener('click', (event) => {
     alert('Tervist!')
     console.log(event);
+});
+
+fruitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const form = new FormData(event.target);
+    const value = form.get("fruit");
+
+    if(fruits.includes(value)) {
+        errorMessageSpan.innerText = "Value already in list."
+        return;
+    };
+
+    errorMessageSpan.innerText = "";
+
+    fruits.push(value);
+    renderFruitList();
+
+    fruitForm.reset();
 });
