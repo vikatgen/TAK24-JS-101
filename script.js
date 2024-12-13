fb9cec620461a011e969a0adfaba1fbc0a4943c8
Gen Vikat <vikat.gen@gmail.com>
2024-12-13 13:44:50 +0200

    adds feature to delete fruits from list, using single event listener

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ function renderFruitList() {
     olElement.innerHTML = "";
     for(let i = 0; i < fruits.length; i++) {
         const listElement = document.createElement('li');
+        listElement.classList.add("list-element");
         listElement.innerText = fruits[i];
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "🗑️";
+        deleteButton.setAttribute("data-index", i);
+
+        listElement.appendChild(deleteButton);
         olElement.appendChild(listElement);
     }
 };
 fruitListElement.appendChild(olElement);
 
 renderFruitList();
-
-
 
 welcomeButton.classList.add('primary-button');
 welcomeButton.addEventListener('click', (event) => {
@@ -44,4 +49,14 @@ fruitForm.addEventListener("submit", (event) => {
     renderFruitList();
 
     fruitForm.reset();
+});
+
+fruitListElement.addEventListener("click", (event) => {
+    if(event.target.nodeName !== "BUTTON") return; 
+
+    const index = event.target.getAttribute("data-index");
+    
+    fruits.splice(index, 1);
+    
+    renderFruitList();
 });
